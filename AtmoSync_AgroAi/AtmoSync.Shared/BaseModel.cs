acdab931e1abf4907615ce6b3a831a07fa59d976
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-11 08:37:15 +0600

    Add Class Library

---
+++ AtmoSync_AgroAi/AtmoSync.Shared/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmoSync.Shared
+{
+    public class BaseModel
+    {
+        public string CreatedBy { get; set; } = "Fahad";
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool InActive { get; set; } = false;
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
---
+++ AtmoSync_AgroAi/AtmoSync.Shared/Models/DtoModels/DHTSensorDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmoSync.Shared.Models.DtoModels
+{
+    internal class DHTSensorDto
+    {
+    }
+}

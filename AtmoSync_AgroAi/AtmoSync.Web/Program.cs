73f0a578c1ef85cc924d0df735ac6908c49ea8c7
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-26 18:36:51 +0600

    Add Ui View ON MQ136

--- AtmoSync_AgroAi/AtmoSync.Web/Program.cs
+++ AtmoSync_AgroAi/AtmoSync.Web/Program.cs
@@ -10,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7276/api/") });
 
 builder.Services.AddScoped<DhtSensorApiService>();
+builder.Services.AddScoped<MQ136SensorApiService>();
+builder.Services.AddScoped<MQ7SensorApiService>();
 
 await builder.Build().RunAsync();
--- AtmoSync_AgroAi/AtmoSync.Web/Services/MQ7SensorApiService.cs
+++ AtmoSync_AgroAi/AtmoSync.Web/Services/MQ7SensorApiService.cs
@@ -1,6 +1,112 @@
-﻿namespace AtmoSync.Web.Services
+﻿using AtmoSync.Shared;
+using AtmoSync.Shared.Models.DtoModels;
+using System.Net.Http.Json;
+
+namespace AtmoSync.Web.Services
 {
     public class MQ7SensorApiService
     {
+
+        private readonly HttpClient _httpClient;
+
+        public MQ7SensorApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        //Get All
+        public async Task<ResponseModel<List<MQ7SensorDto>>?> GetAllAsync()
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<ResponseModel<List<MQ7SensorDto>>>("MQ7Sensor");
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        // Get LATEST
+        public async Task<ResponseModel<MQ7SensorDto>?> GetLatestAsync()
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<ResponseModel<MQ7SensorDto>>("MQ7Sensor/latest");
+            }
+            catch (Exception )
+            {
+                return null;
+            }
+        }
+        // GET LAST N
+        public async Task<ResponseModel<List<MQ7SensorDto>>?> GetLatestReadingsAsync(int count)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<ResponseModel<List<MQ7SensorDto>>>($"MQ7Sensor/latest/{count}");
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        // CREATE
+        public async Task<ResponseModel<long>?> CreateAsync(MQ7SensorDto dto)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("MQ7Sensor", dto);
+
+                return await response.Content.ReadFromJsonAsync<ResponseModel<long>>();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<ResponseModel<List<MQ7SensorDto>>?> GetByDateRangeAsync(DateTime fromDate, DateTime toDate)
+        {
+            try
+            {
+                var url = $"MQ7Sensor/range?fromDate={fromDate:yyyy-MM-dd}&toDate={toDate:yyyy-MM-dd}";
+
+                return await _httpClient.GetFromJsonAsync<ResponseModel<List<MQ7SensorDto>>>(url);
+            }
+            catch (Exception )
+            {
+                return null;
+            }
+        }
+
+        // DELETE
+        public async Task<ResponseModel<int>?> DeleteAsync(long id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"MQ7Sensor/{id}");
+
+                return await response.Content.ReadFromJsonAsync<ResponseModel<int>>();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public async Task<ResponseModel<int>?> UpdateStatusAsync(long id, bool inActive)
+        {
+            try
+            {
+                var response = await _httpClient.PutAsync($"MQ7Sensor/{id}/status?inActive={inActive}", null);
+
+                return await response.Content.ReadFromJsonAsync<ResponseModel<int>>();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
     }
 }

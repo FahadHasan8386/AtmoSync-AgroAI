b4af91cb3bdf9a440f243f090a47d4e06f3bca89
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-28 18:44:46 +0600

    Add CustomAuthStateProvider for Authentication

---
+++ AtmoSync_AgroAi/AtmoSync.Web/Services/CustomAuthStateProvider .cs
@@ -0,0 +1,23 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace AtmoSync.Web.Services
+{
+    //public class CustomAuthStateProvider : AuthenticationStateProvider
+    //{
+    //    private readonly ILocalStorageService _localStorage;
+
+    //    private readonly ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+    //    public CustomAuthStateProvider(ILocalStorageService localStorage)
+    //    {
+    //        _localStorage = localStorage;
+    //    }
+
+
+    //    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    //    {
+    //        string? Token = null;
+    //    }
+    //}
+}

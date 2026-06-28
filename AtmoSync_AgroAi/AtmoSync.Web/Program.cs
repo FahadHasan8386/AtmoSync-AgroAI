cff7c9f4eabfe939bd1d74bcb0ed4d94fd18a7d0
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-28 19:20:37 +0600

    Fix some Bugs on Authentication

--- AtmoSync_AgroAi/AtmoSync.Web/Program.cs
+++ AtmoSync_AgroAi/AtmoSync.Web/Program.cs
@@ -1,5 +1,7 @@
 using AtmoSync.Web;
 using AtmoSync.Web.Services;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,9 +11,19 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7276/api/") });
 
+// LocalStorage register 
+builder.Services.AddBlazoredLocalStorage();
+
+// Authorization 
+builder.Services.AddAuthorizationCore();
+
 builder.Services.AddScoped<DhtSensorApiService>();
 builder.Services.AddScoped<MQ136SensorApiService>();
 builder.Services.AddScoped<MQ7SensorApiService>();
+builder.Services.AddScoped<CustomAuthStateProvider>();
+
+builder.Services.AddScoped<AuthenticationStateProvider>(
+    sp => sp.GetRequiredService<CustomAuthStateProvider>());
 builder.Services.AddScoped<AuthApiService>();
 
 await builder.Build().RunAsync();
--- AtmoSync_AgroAi/AtmoSync.Web/Services/AuthApiService.cs
+++ AtmoSync_AgroAi/AtmoSync.Web/Services/AuthApiService.cs
@@ -1,5 +1,8 @@
 ﻿using AtmoSync.Shared;
 using AtmoSync.Shared.Models.DtoModels;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace AtmoSync.Web.Services
@@ -7,10 +10,15 @@ namespace AtmoSync.Web.Services
     public class AuthApiService
     {
         private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+        private readonly CustomAuthStateProvider _authStateProvider;
 
-        public AuthApiService (HttpClient httpClient)
+        public AuthApiService(HttpClient httpClient,ILocalStorageService localStorage,AuthenticationStateProvider authStateProvider)
         {
             _httpClient = httpClient;
+            _localStorage = localStorage;
+
+            _authStateProvider = (CustomAuthStateProvider)authStateProvider;
         }
 
         public async Task<ResponseModel<string>?> RegisterAsync(RegisterDto dto)
@@ -39,14 +47,36 @@ namespace AtmoSync.Web.Services
                     "Auth/login",
                     dto);
 
-                return await response.Content
+                var result = await response.Content
                     .ReadFromJsonAsync<ResponseModel<LoginResponseDto>>();
+
+                if (result is not null && result.Code == 200 && result.Data is not null && !string.IsNullOrWhiteSpace(result.Data.Token))
+                {
+                    var token = result.Data.Token;
+
+                    await _localStorage.SetItemAsStringAsync("authToken", token);
+
+                    // CustomAuthStateProvider knows user is authenticated
+                    _authStateProvider.MarkUserAsAuthenticated(token);
+
+                    _httpClient.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", token);
+                }
+
+                return result;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 return null;
             }
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            _authStateProvider.MarkUserAsLoggedOut();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
         }
     }
 }

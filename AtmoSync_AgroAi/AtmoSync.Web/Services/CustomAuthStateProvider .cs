1494f100d0041b26c850ab69ab0cf4c78b1a72ee
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-28 18:52:17 +0600

    Add CustomAuthStateProvider for Authentication

--- AtmoSync_AgroAi/AtmoSync.Web/Services/CustomAuthStateProvider .cs
+++ AtmoSync_AgroAi/AtmoSync.Web/Services/CustomAuthStateProvider .cs
@@ -1,23 +1,66 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace AtmoSync.Web.Services
 {
-    //public class CustomAuthStateProvider : AuthenticationStateProvider
-    //{
-    //    private readonly ILocalStorageService _localStorage;
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
 
-    //    private readonly ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
-    //    public CustomAuthStateProvider(ILocalStorageService localStorage)
-    //    {
-    //        _localStorage = localStorage;
-    //    }
+        private readonly ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
 
 
-    //    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
-    //    {
-    //        string? Token = null;
-    //    }
-    //}
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string? token = null;
+            try
+            {
+                token = await _localStorage.GetItemAsStringAsync("authToken");
+            }
+            catch
+            {
+
+            }
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return new AuthenticationState(_anonymous);
+            }
+
+            token = token.Trim('"');
+
+            var claims = ParseClaimsFromJwt(token);
+            var identity = new ClaimsIdentity(claims, "jwt");
+            var user = new ClaimsPrincipal(identity);
+
+            return new AuthenticationState(user);
+        }
+
+        public void MarkUserAsAuthenticated(string token)
+        {
+            var claims = ParseClaimsFromJwt(token);
+            var identity = new ClaimsIdentity(claims, "jwt");
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_anonymous)));
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(jwt);
+            return token.Claims;
+        }
+    }
 }

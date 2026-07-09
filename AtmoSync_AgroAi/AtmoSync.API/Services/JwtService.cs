d512f931be2c67571a36e3ac00cadecadf6c2149
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-09 20:20:44 +0600

    Add Jwt Service

--- AtmoSync_AgroAi/AtmoSync.API/Services/JwtService.cs
+++ AtmoSync_AgroAi/AtmoSync.API/Services/JwtService.cs
@@ -1,15 +1,15 @@
 ﻿using AtmoSync.API.Model;
-using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace AtmoSync.API.Services
 {
     public class JwtService
     {
         private readonly IConfiguration _configuration;
-
         public JwtService(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -19,44 +19,26 @@ namespace AtmoSync.API.Services
         {
             var claims = new[]
             {
-            new Claim(ClaimTypes.NameIdentifier,
-                user.Id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim( ClaimTypes.Name,user.FullName),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Role,user.Role)
 
-            new Claim(ClaimTypes.Email,
-                user.Email),
+                };
 
-            new Claim(ClaimTypes.Role,
-                user.Role)
-        };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes( _configuration["Jwt:Key"]!));
 
-            var key =
-                new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(
-                        _configuration["Jwt:Key"]!));
+            var credential = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
 
-            var credentials =
-                new SigningCredentials(
-                    key,
-                    SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(1),
+            signingCredentials: credential
 
-            var token =
-                new JwtSecurityToken(
-                    issuer:
-                        _configuration["Jwt:Issuer"],
-
-                    audience:
-                        _configuration["Jwt:Audience"],
-
-                    claims: claims,
-
-                    expires:
-                        DateTime.UtcNow.AddDays(7),
-
-                    signingCredentials:
-                        credentials);
-
-            return new JwtSecurityTokenHandler()
-                .WriteToken(token);
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }

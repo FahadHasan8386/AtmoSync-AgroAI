f2bc97ade4d46d460ddd046e11b86373bba32a56
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-11 00:30:21 +0600

    Add ApiAuthenticationHeaderHandler

--- AtmoSync_AgroAi/AtmoSync.Web/Services/ApiAuthenticationHeaderHandler.cs
+++ AtmoSync_AgroAi/AtmoSync.Web/Services/ApiAuthenticationHeaderHandler.cs
@@ -1,6 +1,26 @@
-﻿namespace AtmoSync.Web.Services
+﻿using System.Net.Http.Headers;
+
+namespace AtmoSync.Web.Services
 {
-    public class ApiAuthenticationHeaderHandler
+    public class ApiAuthenticationHeaderHandler : DelegatingHandler
     {
+        private readonly TokenService _tokenService;
+
+        public ApiAuthenticationHeaderHandler(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _tokenService.GetAccessTokenAsync();
+
+            if(!string.IsNullOrWhiteSpace(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
     }
 }
